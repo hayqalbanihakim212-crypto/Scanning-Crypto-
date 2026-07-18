90b2cf7d7e186818da299e55e7b43b09b1f89184
hayqalbanihakim212-crypto <hayqalbanihakim212@gmail.com>
2026-07-18 10:54:25 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: '/Scanning-Crypto-/',
   plugins: [
     react({
       include: /\.(mdx|js|jsx|ts|tsx)$/,
